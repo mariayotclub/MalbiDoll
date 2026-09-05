f8aa7a5ccdc2ddeee7b2c68b1c2b818851d69b49
Maria Luisa <marialuisarb@pop-os.local>
2026-09-05 19:11:07 -0300

    cinco

--- public/scripts/trocarCat.js
+++ public/scripts/trocarCat.js
@@ -1,3 +1,12 @@
-function trocarCat(){
-    
+let catAtual= null;
+
+function trocarCat(botaoClick){
+    catAtual=botaoClick.id;
+
+    document.querySelectorAll('[id^="cat"]').forEach(cat=> {
+        cat.style.display= "none";
+    });
+
+    document.getElementById("cat" + catAtual).style.display= "block";
 }
+    trocarCat(document.getElementById("Corpo"));
