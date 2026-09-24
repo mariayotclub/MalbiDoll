22c79a80947cc6b617dbec88bdeac02b0e40de58
mariayotclub <marialuisabalim978@gmail.com>
2026-09-24 09:59:24 -0300

    oito

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import itensRoutes from "./routes/itensRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(itensRoutes);
 
 app.get("/", (req, res) => {
     res.render("armario");
